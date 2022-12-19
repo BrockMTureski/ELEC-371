55d7efd0139fbff5854e051abfc9abb2a0891595
Brock Tureski <91154875+BrockMTureski@users.noreply.github.com>
2022-12-18 19:56:28 -0500

    Update exceptionHandler.s

--- exceptionHandler.s
+++ exceptionHandler.s
@@ -1,25 +1,25 @@
 ISR:
-	stw		r2,0(sp)
-	stw		r3,4(sp)
+	stw	r2,0(sp)
+	stw	r3,4(sp)
 	subi	sp,sp,8
 	
 	rdctl	et,ipending
 	movi	r2,0x10
-	and		r3,r2,et
-	bne		r3,r2,HandleY
+	and	r3,r2,et
+	bne	r3,r2,HandleY
 	
 	call	HandleIntX
 	
 HandleY:
 	movi	r2,0x20
-	and		r3,r2,et
-	bne		r3,r2,Exit
+	and	r3,r2,et
+	bne	r3,r2,Exit
 	
 	call	HandleIntY
 	
 Exit:
 	addi	sp,sp,8
-	ldw		r2,0(sp)
-	ldw		r3,4(sp)
+	ldw	r2,0(sp)
+	ldw	r3,4(sp)
 	
 	eret
